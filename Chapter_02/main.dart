02ec707bb0acab473b80b3210a78f209dc8914a8
Tracey Powers <traceyp@google.com>
2013-10-14 11:09:34 -0700

    rearranging some code

--- Chapter_02/main.dart
+++ Chapter_02/main.dart
@@ -19,12 +19,41 @@ import 'package:angular/angular.dart';
     publishAs: 'ctrl')
 class RecipeBookController {
 
-  List recipes = _loadData();
+  List recipes;
+  RecipeBookController() {
+    recipes = _loadData();
+  }
 
   Recipe selectedRecipe;
 
   void selectRecipe(Recipe recipe) {
     selectedRecipe = recipe;
+  }
+
+  List<Recipe> _loadData() {
+    return [
+      new Recipe('My Appetizer','Appetizers',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 1),
+      new Recipe('My Salad','Salads',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+      new Recipe('My Soup','Soups',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 4),
+      new Recipe('My Main Dish','Main Dishes',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 2),
+      new Recipe('My Side Dish','Side Dishes',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+      new Recipe('My Awesome Dessert','Desserts',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 5),
+      new Recipe('My So-So Dessert','Desserts',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+          ];
   }
 }
 
@@ -37,32 +66,6 @@ class Recipe {
 
   Recipe(this.name, this.category, this.ingredients, this.directions,
       this.rating);
-}
-
-List<Recipe> _loadData() {
-  return [
-       new Recipe('My Appetizer','Appetizers',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 1),
-       new Recipe('My Salad','Salads',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 3),
-       new Recipe('My Soup','Soups',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 4),
-       new Recipe('My Main Dish','Main Dishes',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 2),
-       new Recipe('My Side Dish','Side Dishes',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 3),
-       new Recipe('My Awesome Dessert','Desserts',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 5),
-       new Recipe('My So-So Dessert','Desserts',
-           ["Ingredient 1", "Ingredient 2"],
-           "Some Directions", 3),
-       ];
 }
 
 main() {
