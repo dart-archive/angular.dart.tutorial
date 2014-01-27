266ed3cd40e99ca8d92268afe500fc364c5f4682
Pavel Jbanov <pavelj@google.com>
2014-01-27 15:12:09 -0500

    fix(Chapter 7): fixed parser gen header file

--- Chapter_07/lib/parser_gen_header.dart
+++ Chapter_07/lib/parser_gen_header.dart
@@ -1,6 +1,6 @@
 library parser_gen;
 
 import 'package:angular/angular.dart';
-import 'package:angular/utils.dart';
+import 'package:angular/core/parser/utils.dart';
 
 typedef Function FilterLookup(String filterName);
