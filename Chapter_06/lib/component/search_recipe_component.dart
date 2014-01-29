a21a45d9a561d8c24efc9028139ad19e75da1cf6
Patrice Chalin <pchalin@gmail.com>
2014-01-28 22:44:42 -0600

    Drop cssUrl parameter to NgComponent since .css is empty (Chap. 6-7)
    The component folder holds the files for the two components
    search_recipe and view_recipe. Given that one of these has an empty
    .css file, it seems reasonable to drop it, reducing the clutter and
    driving home the fact that the `cssUrl` parameter is optional.

--- Chapter_06/lib/component/search_recipe_component.dart
+++ Chapter_06/lib/component/search_recipe_component.dart
@@ -5,7 +5,6 @@ import 'package:angular/angular.dart';
 @NgComponent(
     selector: 'search-recipe',
     templateUrl: 'packages/angular_dart_demo/component/search_recipe_component.html',
-    cssUrl: 'packages/angular_dart_demo/component/search_recipe_component.css',
     publishAs: 'ctrl',
     map: const {
       'name-filter-string': '<=>nameFilterString',
