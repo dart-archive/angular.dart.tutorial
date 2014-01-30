e303c772cd0e956492d2d1e44b912809596dff47
Kathy Walrath <kathyw@google.com>
2014-01-29 16:25:12 -0800

    Stripped down ch07 imports, @MirrorsUsed
    I'm going to commit, since this removes unnecessary code (yay!).
    
    Review URL: https://codereview.appspot.com/58460043

--- Chapter_07/web/main.dart
+++ Chapter_07/web/main.dart
@@ -7,31 +7,14 @@ library recipe_book;
 // Used by dart2js to indicate which targets are being reflected on, to allow
 // tree-shaking.
 @MirrorsUsed(
-    targets: const [
-        'angular.core',
-        'angular.core.dom',
-        'angular.core.parser',
-        'angular.routing',
-        'NodeTreeSanitizer'
-    ],
     metaTargets: const [
-        NgInjectableService,
-        NgComponent,
-        NgDirective,
-        NgController,
-        NgFilter,
-        NgAttr,
-        NgOneWay,
-        NgOneWayOneTime,
-        NgTwoWay,
-        NgCallback
+        NgFilter
     ],
     override: '*'
 )
 import 'dart:mirrors';
 
 import 'dart:async';
-import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular/routing/module.dart';
 import 'package:di/di.dart';
@@ -39,13 +22,11 @@ import 'package:di/annotations.dart';
 import 'package:logging/logging.dart';
 import 'package:perf_api/perf_api.dart';
 
-import 'package:angular_dart_demo/injectable.dart';
 import 'package:angular_dart_demo/recipe_book.dart';
 import 'package:angular_dart_demo/filter/category_filter.dart';
 import 'package:angular_dart_demo/rating/rating_component.dart';
 import 'package:angular_dart_demo/tooltip/tooltip_directive.dart';
 import 'package:angular_dart_demo/service/query_service.dart';
-import 'package:angular_dart_demo/service/recipe.dart';
 import 'package:angular_dart_demo/routing/recipe_book_router.dart';
 import 'package:angular_dart_demo/component/view_recipe_component.dart';
 import 'package:angular_dart_demo/component/search_recipe_component.dart';
