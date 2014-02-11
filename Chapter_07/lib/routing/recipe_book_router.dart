76a9f852804a34ff321fa1d162d98a44c117aa74
Adam Singer <financeCoding@gmail.com>
2014-02-11 15:07:47 -0800

    4 spaces indentation

--- Chapter_07/lib/routing/recipe_book_router.dart
+++ Chapter_07/lib/routing/recipe_book_router.dart
@@ -3,28 +3,28 @@ library recipe_book_routing;
 import 'package:angular/angular.dart';
 
 RouteInitializerFn recipeBookRouteInitializer =
-  (Router router, ViewFactory view) {
-    router.root
-      ..addRoute(
-          name: 'add',
-          path: '/add',
-          enter: view('view/addRecipe.html'))
-      ..addRoute(
-          name: 'recipe',
-          path: '/recipe/:recipeId',
-          mount: (Route route) => route
-              ..addRoute(
-                  name: 'view',
-                  path: '/view',
-                  enter: view('view/viewRecipe.html'))
-              ..addRoute(
-                  name: 'edit',
-                  path: '/edit',
-                  enter: view('view/editRecipe.html'))
-              ..addRoute(
-                  name: 'view_default',
-                  defaultRoute: true,
-                  enter: (_) =>
-                      router.go('view', {'recipeId': ':recipeId'},
-                          startingFrom: route, replace:true)));
-  };
+    (Router router, ViewFactory view) {
+      router.root
+        ..addRoute(
+            name: 'add',
+            path: '/add',
+            enter: view('view/addRecipe.html'))
+        ..addRoute(
+            name: 'recipe',
+            path: '/recipe/:recipeId',
+            mount: (Route route) => route
+                ..addRoute(
+                    name: 'view',
+                    path: '/view',
+                    enter: view('view/viewRecipe.html'))
+                ..addRoute(
+                    name: 'edit',
+                    path: '/edit',
+                    enter: view('view/editRecipe.html'))
+                ..addRoute(
+                    name: 'view_default',
+                    defaultRoute: true,
+                    enter: (_) =>
+                        router.go('view', {'recipeId': ':recipeId'},
+                            startingFrom: route, replace:true)));
+      };
