dbfd11afeae43c385096dd9611c792f7ce195597
Adam Singer <financeCoding@gmail.com>
2014-02-11 15:12:33 -0800

    Removed Profiler

--- Chapter_03/web/main.dart
+++ Chapter_03/web/main.dart
@@ -2,7 +2,6 @@ library recipe_book;
 
 import 'package:angular/angular.dart';
 import 'package:di/di.dart';
-import 'package:perf_api/perf_api.dart';
 
 import 'package:angular_dart_demo/rating/rating_component.dart';
 import 'package:angular_dart_demo/recipe_book.dart';
@@ -15,7 +14,6 @@ class MyAppModule extends Module {
   MyAppModule() {
     type(RecipeBookController);
     type(RatingComponent);
-    type(Profiler, implementedBy: Profiler); // uncomment to enable profiling
   }
 }
 
