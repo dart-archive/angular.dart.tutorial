6bb5a85acb179fe10728d38140ddf8d343f399a9
Victor <victor@suumit.com>
2014-02-28 16:10:18 +0100

    cleanup routing

--- Chapter_07/lib/routing/recipe_book_router.dart
+++ Chapter_07/lib/routing/recipe_book_router.dart
@@ -2,25 +2,26 @@ library recipe_book_routing;
 
 import 'package:angular/angular.dart';
 
-recipeBookRouteInitializer(Router router, ViewFactory views) =>
-    views.configure({
-      'add': ngRoute(
-          path: '/add',
-          view: 'view/addRecipe.html'),
-      'recipe': ngRoute(
-          path: '/recipe/:recipeId',
-          mount: {
-            'view': ngRoute(
-                path: '/view',
-                view: 'view/viewRecipe.html'),
-            'edit': ngRoute(
-                path: '/edit',
-                view: 'view/editRecipe.html'),
-            'view_default': ngRoute(
-                defaultRoute: true,
-                enter: (RouteEnterEvent e) =>
-                    router.go('view', {},
-                        startingFrom: router.root.getRoute('recipe'),
-                        replace: true))
-          })
-    });
+void recipeBookRouteInitializer(Router router, ViewFactory views) {
+  views.configure({
+    'add': ngRoute(
+        path: '/add',
+        view: 'view/addRecipe.html'),
+    'recipe': ngRoute(
+        path: '/recipe/:recipeId',
+        mount: {
+          'view': ngRoute(
+              path: '/view',
+              view: 'view/viewRecipe.html'),
+          'edit': ngRoute(
+              path: '/edit',
+              view: 'view/editRecipe.html'),
+          'view_default': ngRoute(
+              defaultRoute: true,
+              enter: (RouteEnterEvent e) =>
+                  router.go('view', {},
+                      startingFrom: router.root.getRoute('recipe'),
+                      replace: true))
+        })
+  });
+}
