2f8b694d2b91f2c008e082f227973433c369dc22
Victor <victor@suumit.com>
2014-02-28 15:49:06 +0100

    rating component cleanup

--- Chapter_07/lib/rating/rating_component.dart
+++ Chapter_07/lib/rating/rating_component.dart
@@ -31,35 +31,33 @@ import 'package:angular/angular.dart';
     selector: 'rating',
     templateUrl: 'packages/angular_dart_demo/rating/rating_component.html',
     cssUrl: 'packages/angular_dart_demo/rating/rating_component.css',
-    publishAs: 'ctrl'
-)
+    publishAs: 'cmp')
 class RatingComponent {
-  static const String _starOnChar = "\u2605";
-  static const String _starOffChar = "\u2606";
-  static const String _starOnClass = "star-on";
-  static const String _starOffClass = "star-off";
+  static const String _STAR_ON_CHAR = "\u2605";
+  static const String _STAR_OFF_CHAR = "\u2606";
+  static const String _STAR_ON_CLASS = "star-on";
+  static const String _STAR_OFF_CLASS = "star-off";
 
   static final int DEFAULT_MAX = 5;
-  
+
   List<int> stars = [];
 
   @NgTwoWay('rating')
   int rating;
 
   @NgAttr('max-rating')
-  set maxRating(String value) {
-    var count = value == null ? DEFAULT_MAX : 
-        int.parse(value, onError: (_) => DEFAULT_MAX);
-    stars = new List.generate(count, (i) => i+1);
+  void set maxRating(String value) {
+    var count = value == null
+        ? DEFAULT_MAX
+        : int.parse(value, onError: (_) => DEFAULT_MAX);
+    stars = new List.generate(count, (i) => i + 1);
   }
 
   String starClass(int star) {
-    return star > rating ? _starOffClass : _starOnClass;
+    return star > rating ? _STAR_OFF_CLASS : _STAR_ON_CLASS;
   }
 
-  String starChar(int star) {
-    return star > rating ? _starOffChar : _starOnChar;
-  }
+  String starChar(int star) => star > rating ? _STAR_OFF_CHAR : _STAR_ON_CHAR;
 
   void handleClick(int star) {
     rating = (star == 1 && rating == 1) ? 0 : star;
