527fa1738211a646702d2a3aae6ff02d7c7e1db4
Patrice Chalin <pchalin@gmail.com>
2014-03-05 20:37:48 -0600

    fix(recipe): follow new dart:convert.JsonCodec JSON encoding convention
    - `recipe.dart` of Chap. 5:
      - Replace `toJsonString()` by `Map toJson()` as documented in
    [dart:convert.JsonCodec#encode](https://api.dartlang.org/apidocs/channel
    s/stable/#dart-convert.JsonCodec@id_encode)
      - Some code cleanup.
    - Chap. 6 & 7 made to match updates to Chap. 5.

--- Chapter_07/lib/service/recipe.dart
+++ Chapter_07/lib/service/recipe.dart
@@ -1,7 +1,5 @@
 library recipe;
 
-import 'dart:convert';
-
 class Recipe {
   String id;
   String name;
@@ -11,25 +9,19 @@ class Recipe {
   int rating;
   String imgUrl;
 
-  Recipe(this.id, this.name, this.category, this.ingredients, this.directions,
-      this.rating, this.imgUrl);
+  Recipe(this.id, this.name, this.category, this.ingredients, this.directions, this.rating, this.imgUrl);
 
-  String toJsonString() {
-    Map data = {
-                "id" : id,
-                "name" : name,
-                "category" : category,
-                "ingredients" : ingredients,
-                "directions" : directions,
-                "rating" : rating,
-                "imgUrl" : imgUrl
-    };
-    return JSON.encode(data);
-  }
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    "id": id,
+    "name": name,
+    "category": category,
+    "ingredients": ingredients,
+    "directions": directions,
+    "rating": rating,
+    "imgUrl": imgUrl
+  };
 
-  factory Recipe.fromJsonMap(Map json) {
-    return new Recipe(json['id'], json['name'], json['category'],
-        json['ingredients'], json['directions'], json['rating'],
-        json['imgUrl']);
-  }
+  factory Recipe.fromJsonMap(Map<String, dynamic> json) => new Recipe(json['id'], json['name'],
+      json['category'], json['ingredients'], json['directions'], json['rating'],
+      json['imgUrl']);
 }
