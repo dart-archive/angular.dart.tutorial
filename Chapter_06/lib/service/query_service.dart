1248006c67231d792d5d6936df917298f335de75
Kathy Walrath <kathyw@google.com>
2014-04-10 13:32:24 -0700

    Fixes to make Chapter_06 work in JS

--- Chapter_06/lib/service/query_service.dart
+++ Chapter_06/lib/service/query_service.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'recipe.dart';
 import 'package:angular/angular.dart';
 
+@NgInjectableService()
 class QueryService {
   String _recipesUrl = 'recipes.json';
   String _categoriesUrl = 'categories.json';
