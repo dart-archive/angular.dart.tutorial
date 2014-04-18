5d2a17658c7c56d7aaf65f7f2432896e5394a057
smallg <gwpxjb@163.com>
2014-04-18 10:06:44 +0800

    Update main_test.dart
    fix error file location

--- Chapter_06/test/main_test.dart
+++ Chapter_06/test/main_test.dart
@@ -5,7 +5,7 @@ import 'package:di/di.dart';
 import 'package:angular/angular.dart';
 import 'package:angular/mock/module.dart';
 import 'package:angular_dart_demo/recipe_book.dart';
-import 'package:angular_dart_demo/filter/category_filter.dart';
+import 'package:angular_dart_demo/formatter/category_filter.dart';
 import 'package:angular_dart_demo/rating/rating_component.dart';
 import 'package:angular_dart_demo/service/recipe.dart';
 
